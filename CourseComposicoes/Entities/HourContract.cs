d388a22453df80ddab13012515a069a46365e73f
FAARJSI <faarjsi@gmail.com>
2019-03-25 20:14:21 -0300

    Criando classe HourContact

--- CourseComposicoes/Entities/HourContract.cs
+++ CourseComposicoes/Entities/HourContract.cs
@@ -4,13 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CourseComposicoes
+namespace CourseComposicoes.Entities
 {
-    class Program
+    class HourContract
     {
-        static void Main(string[] args)
-        {
-
-        }
     }
 }
